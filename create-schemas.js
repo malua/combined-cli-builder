a1e2231a3cf6b7058beeae2ba579e1beddca80d7
Lukas Mayr <malua@posteo.net>
2022-08-18 13:38:05 +0200

    fix: readded create schema

---
+++ create-schemas.js
@@ -0,0 +1,36 @@
+var rootPath = process.cwd();
+(fs = require("fs")),
+  (path = require("path")),
+  (defaultsDeep = require("lodash/defaultsDeep")),
+  (angularSchemas = ["browser", "dev-server", "server"]);
+
+function createModifiedAngularSchema(name) {
+  let original = {};
+  try {
+    original = require(`@angular-devkit/build-angular/src/builders/${name}/schema.json`);
+  } catch (e) {
+    original = require(`@angular-devkit/build-angular/src/${name}/schema.json`);
+  }
+  const modifications = require(`./src/${name}/schema.json`);
+
+  createSchema(name, original, modifications);
+}
+
+function createSchema(name, original, modifications) {
+  const result = defaultsDeep(original, modifications),
+    dest = path.resolve(rootPath, `./dist/${name}`),
+    data = JSON.stringify(result, null, 2);
+
+  if (fs.existsSync(dest)) {
+    fs.writeFile(`${dest}/schema.json`, data, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`Created ${dest}/schema.json`);
+    });
+  } else {
+    console.log(`Path does not exist: ${dest}`);
+  }
+}
+
+angularSchemas.forEach((s) => createModifiedAngularSchema(s));
